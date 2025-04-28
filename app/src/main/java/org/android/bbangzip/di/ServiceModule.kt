0707bf4a710657f4deeefb8c0fdd842322a1bc47
JIEUNI <jieundaeun@naver.com>
2025-04-28 21:14:07 +0900

    [init] #1 Hilt 모듈 추가

---
+++ app/src/main/java/org/android/bbangzip/di/ServiceModule.kt
@@ -0,0 +1,20 @@
+package org.android.bbangzip.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.android.bbangzip.data.service.DummyService
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideDummyService(
+        @BbangZip retrofit: Retrofit,
+    ): DummyService =
+        retrofit.create(DummyService::class.java)
+}
