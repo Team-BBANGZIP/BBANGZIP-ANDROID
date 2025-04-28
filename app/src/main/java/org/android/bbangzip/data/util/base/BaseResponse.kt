265984a39baabe2bc1d9cb37b0507f8de9d43277
JIEUNI <jieundaeun@naver.com>
2025-04-28 21:12:01 +0900

    [init] #1 BaseResponse 추가

---
+++ app/src/main/java/org/android/bbangzip/data/util/base/BaseResponse.kt
@@ -0,0 +1,15 @@
+package org.android.bbangzip.data.util.base
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class BaseResponse<T>(
+    @SerialName("code")
+    val code: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: T,
+)
