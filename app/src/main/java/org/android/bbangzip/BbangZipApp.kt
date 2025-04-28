9ba1a40aa822effa6be35b08248ff972aed68ffd
JIEUNI <jieundaeun@naver.com>
2025-04-28 21:02:43 +0900

    [init] #1 Timber, Kakao 설정 및 다크모드 비활성화

---
+++ app/src/main/java/org/android/bbangzip/BbangZipApp.kt
@@ -0,0 +1,30 @@
+package org.android.bbangzip
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class BbangZipApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        setDarkMode()
+        setTimber()
+        setKakao()
+    }
+
+    private fun setDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun setTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setKakao() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
