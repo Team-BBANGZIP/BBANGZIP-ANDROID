4f2c5a5a16cc88221d2b0270ec3246c38943fd0f
beom84 <cjja00@konkuk.ac.kr>
2025-05-07 02:52:50 +0900

    [feat] #3 BottomBar navigation 연결

--- app/src/main/java/org/android/bbangzip/presentation/ui/navigator/MainNavigator.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/navigator/MainNavigator.kt
@@ -13,6 +13,11 @@ import org.android.bbangzip.presentation.model.BottomNavigationRoute
 import org.android.bbangzip.presentation.model.Route
 import org.android.bbangzip.presentation.type.BottomNavigationType
 import org.android.bbangzip.presentation.ui.dummy.navigation.navigateDummy
+import org.android.bbangzip.presentation.ui.friend.navigation.navigateToFriend
+import org.android.bbangzip.presentation.ui.my.navigation.navigateToMy
+import org.android.bbangzip.presentation.ui.timer.navigation.navigateToTimer
+import org.android.bbangzip.presentation.ui.todo.navigation.navigateToTodo
+import timber.log.Timber
 
 class MainNavigator(
     val navHostController: NavHostController,
@@ -20,7 +25,7 @@ class MainNavigator(
     private val currentDestination: NavDestination?
         @Composable get() = navHostController.currentBackStackEntryAsState().value?.destination
 
-    val startDestination = BottomNavigationRoute.Dummy
+    val startDestination = BottomNavigationRoute.Timer
 
     val currentBottomNavigationBarItem: BottomNavigationType?
         @Composable get() =
@@ -30,15 +35,21 @@ class MainNavigator(
 
     @SuppressLint("RestrictedApi")
     fun navigateBottomNavigation(bottomNavigationType: BottomNavigationType) {
+        Timber.d("[navigation] currentDestination -> ${navHostController.currentDestination}")
         navOptions {
-            popUpTo(BottomNavigationRoute.Dummy::class.qualifiedName.orEmpty()) {
+            popUpTo(BottomNavigationRoute.Timer::class.qualifiedName.orEmpty()) {
                 saveState = true
+                Timber.d("[navigation] restoreState -> $saveState")
             }
             launchSingleTop = true
             restoreState = true
+            Timber.d("[navigation] restoreState -> $restoreState")
         }.let { navOptions ->
             when (bottomNavigationType) {
-                BottomNavigationType.DUMMY -> navigateToDummy(navOptions)
+                BottomNavigationType.TIMER -> navigateToTimer(navOptions)
+                BottomNavigationType.TODO -> navigateToTodo(navOptions)
+                BottomNavigationType.FRIEND -> navigateToFriend(navOptions)
+                BottomNavigationType.MY -> navigateToMy(navOptions)
                 else -> Unit
             }
         }
@@ -46,6 +57,22 @@ class MainNavigator(
 
     private fun navigateToDummy(navOptions: NavOptions) {
         navHostController.navigateDummy(navOptions)
+    }
+
+    private fun navigateToFriend(navOptions: NavOptions) {
+        navHostController.navigateToFriend(navOptions)
+    }
+
+    private fun navigateToMy(navOptions: NavOptions) {
+        navHostController.navigateToMy(navOptions)
+    }
+
+    private fun navigateToTimer(navOptions: NavOptions) {
+        navHostController.navigateToTimer(navOptions)
+    }
+
+    private fun navigateToTodo(navOptions: NavOptions) {
+        navHostController.navigateToTodo(navOptions)
     }
 
     private fun popBackStack() {
