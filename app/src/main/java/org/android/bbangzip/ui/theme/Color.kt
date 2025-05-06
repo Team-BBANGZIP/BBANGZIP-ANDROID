64aefbb612263d470ebcd474f93cff69783cf9e6
JIEUNI <jieundaeun@naver.com>
2025-05-06 17:59:52 +0900

    [chore] #6 코드리뷰 반영

--- app/src/main/java/org/android/bbangzip/ui/theme/Color.kt
+++ app/src/main/java/org/android/bbangzip/ui/theme/Color.kt
@@ -5,84 +5,84 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 // Common
-val Common100: Color = Color(0xFF121212)
-val Common0: Color = Color(0xFFFFFFFF)
+private val Common100: Color = Color(0xFF121212)
+private val Common0: Color = Color(0xFFFFFFFF)
 
 // Grayscale
-val Grayscale5 = Color(0xFFFDFDFD)
-val Grayscale7 = Color(0xFFF6F6F5)
-val Grayscale10 = Color(0xFFEDECEA)
-val Grayscale20 = Color(0xFFE4E2E0)
-val Grayscale30 = Color(0xFFC9C7C5)
-val Grayscale40 = Color(0xFFB6B4B1)
-val Grayscale50 = Color(0xFFA29D96)
-val Grayscale60 = Color(0xFF9A958F)
-val Grayscale70 = Color(0xFF706A63)
-val Grayscale80 = Color(0xFF6B6560)
-val Grayscale90 = Color(0xFF463D34)
+private val Grayscale5 = Color(0xFFFDFDFD)
+private val Grayscale7 = Color(0xFFF6F6F5)
+private val Grayscale10 = Color(0xFFEDECEA)
+private val Grayscale20 = Color(0xFFE4E2E0)
+private val Grayscale30 = Color(0xFFC9C7C5)
+private val Grayscale40 = Color(0xFFB6B4B1)
+private val Grayscale50 = Color(0xFFA29D96)
+private val Grayscale60 = Color(0xFF9A958F)
+private val Grayscale70 = Color(0xFF706A63)
+private val Grayscale80 = Color(0xFF6B6560)
+private val Grayscale90 = Color(0xFF463D34)
 
 // Brown
-val Brown1 = Color(0xFFC8B5A2)
-val Brown50 = Color(0xFF897869)
-val Brown100 = Color(0xFF4B4137)
+private val Brown1 = Color(0xFFC8B5A2)
+private val Brown50 = Color(0xFF897869)
+private val Brown100 = Color(0xFF4B4137)
 
 // Apricot
-val Apricot1 = Color(0xFFFAF6F3)
-val Apricot50 = Color(0xFFF6F1EE)
-val Apricot100 = Color(0xFFF2EAE4)
+private val Apricot1 = Color(0xFFFAF6F3)
+private val Apricot50 = Color(0xFFF6F1EE)
+private val Apricot100 = Color(0xFFF2EAE4)
 
 // Check Color
-val Todo1 = Color(0xFFEA7152)
-val Todo2 = Color(0xFFF09C86)
-val Todo3 = Color(0xFFFED45C)
-val Todo4 = Color(0xFFF6DDAF)
-val Todo5 = Color(0xFF7A946D)
-val Todo6 = Color(0xFFA2B499)
-val Todo7 = Color(0xFF5C62AC)
-val Todo8 = Color(0xFF8D91C5)
-val Todo9 = Color(0xFF8F63E9)
-val Todo10 = Color(0xFFB79FE8)
+private val Todo1 = Color(0xFFEA7152)
+private val Todo2 = Color(0xFFF09C86)
+private val Todo3 = Color(0xFFFED45C)
+private val Todo4 = Color(0xFFF6DDAF)
+private val Todo5 = Color(0xFF7A946D)
+private val Todo6 = Color(0xFFA2B499)
+private val Todo7 = Color(0xFF5C62AC)
+private val Todo8 = Color(0xFF8D91C5)
+private val Todo9 = Color(0xFF8F63E9)
+private val Todo10 = Color(0xFFB79FE8)
 
 @Immutable
 data class BbangZipColor(
     // Primary
-    val primaryLight_C8B5A2: Color,
-    val primaryNormal_897869: Color,
-    val primaryStrong_4B4137: Color,
+    private val primaryLight_C8B5A2: Color,
+    private val primaryNormal_897869: Color,
+    private val primaryStrong_4B4137: Color,
     // Secondary
-    val secondaryLight_FAF6F3: Color,
-    val secondaryNormal_F6F1EE: Color,
-    val secondaryStrong_F2EAE4: Color,
+    private val secondaryLight_FAF6F3: Color,
+    private val secondaryNormal_F6F1EE: Color,
+    private val secondaryStrong_F2EAE4: Color,
     // Label
-    val labelDisable_E4E2E0: Color,
-    val labelAssistive_C9C7C5: Color,
-    val labelAlternative_A29D96: Color,
-    val labelNeutral_706A63: Color,
-    val labelNormal_6B6560: Color,
-    val labelStrong_463D34: Color,
+    private val labelDisable_E4E2E0: Color,
+    private val labelAssistive_C9C7C5: Color,
+    private val labelAlternative_A29D96: Color,
+    private val labelNeutral_706A63: Color,
+    private val labelNormal_6B6560: Color,
+    private val labelStrong_463D34: Color,
     // Background
-    val backgroundNormal_FFFFFF: Color,
-    val backgroundGray_FDFDFD: Color,
-    val backgroundAlternative_FAF6F3: Color,
-    val backgroundStrong_F2EAE4: Color,
-    val backgroundDimmer_282119_52: Color,
+    private val backgroundNormal_FFFFFF: Color,
+    private val backgroundGray_FDFDFD: Color,
+    private val backgroundAlternative_FAF6F3: Color,
+    private val backgroundStrong_F2EAE4: Color,
+    private val backgroundDimmer_282119_52: Color,
     // Static
-    val staticWhite_FFFFFF: Color,
-    val staticBlack_121212: Color,
+    private val staticWhite_FFFFFF: Color,
+    private val staticBlack_121212: Color,
     // Component
-    val componentIvory_FDFDFD: Color,
-    val componentGrey_FFFFFF: Color,
+    private val componentIvory_FDFDFD: Color,
+    private val componentGrey_FFFFFF: Color,
     // To-do
-    val todoRed1_EA7152: Color,
-    val todoRed2_F09C86: Color,
-    val todoYellow1_FED45C: Color,
-    val todoYellow2_F6DDAF: Color,
-    val todoGreen1_7A946D: Color,
-    val todoGreen2_A2B499: Color,
-    val todoBlue1_5C62AC: Color,
-    val todoBlue2_8D91C5: Color,
-    val todoPurple1_8F63E9: Color,
-    val todoPurple2_B79FE8: Color,
+    private val todoRed1_EA7152: Color,
+    private val todoRed2_F09C86: Color,
+    private val todoYellow1_FED45C: Color,
+    private val todoYellow2_F6DDAF: Color,
+    private val todoGreen1_7A946D: Color,
+    private val todoGreen2_A2B499: Color,
+    private val todoBlue1_5C62AC: Color,
+    private val todoBlue2_8D91C5: Color,
+    private val todoPurple1_8F63E9: Color,
+    private val todoPurple2_B79FE8: Color,
 )
 
 val defaultBbangZipColor =
