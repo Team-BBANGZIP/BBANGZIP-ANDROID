0d4a781484b7b0cb31373de8091dfc57d61c24ba
kamja0510 <kamja990510@gmail.com>
2025-05-08 03:09:16 +0900

    [init] #8 enableEdgeToEdge 적용 및 사용법 구현

--- app/src/main/java/org/android/bbangzip/presentation/ui/navigator/MainActivity.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/navigator/MainActivity.kt
@@ -3,12 +3,15 @@ package org.android.bbangzip.presentation.ui.navigator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import org.android.bbangzip.ui.theme.BBANGZIPANDROIDTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
 
         setContent {
--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/TimerRoute.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/TimerRoute.kt
@@ -1,10 +1,31 @@
 package org.android.bbangzip.presentation.ui.timer
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun TimerRoute(modifier: Modifier = Modifier) {
-    Text("timer")
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
+    )
+
+    Column(
+        modifier = modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text("timer")
+    }
 }
