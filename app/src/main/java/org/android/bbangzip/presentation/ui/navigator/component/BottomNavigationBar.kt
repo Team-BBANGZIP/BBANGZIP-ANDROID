09230a410ca9ac47b234c830d0aaa742f8fbe1c4
beom84 <cjja00@konkuk.ac.kr>
2025-05-08 00:23:36 +0900

    [fix] #3 border top에만 적용

--- app/src/main/java/org/android/bbangzip/presentation/ui/navigator/component/BottomNavigationBar.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/navigator/component/BottomNavigationBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -38,6 +39,8 @@ fun BottomNavigationBar(
     onBottomNaviBarItemSelected: (BottomNavigationType) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val borderColor = BbangZipTheme.color.labelDisable_E4E2E0
+
     AnimatedVisibility(
         visible = isVisible,
         enter = slideInVertically(),
@@ -48,10 +51,15 @@ fun BottomNavigationBar(
                 modifier
                     .fillMaxWidth()
                     .background(color = BbangZipTheme.color.componentIvory_FDFDFD)
-                    .border(
-                        width = 1.dp,
-                        color = BbangZipTheme.color.labelDisable_E4E2E0,
-                    ),
+                    .drawBehind {
+                        val strokeWidth = 1.dp.toPx()
+                        drawLine(
+                            color = borderColor,
+                            start = Offset(0f, 0f),
+                            end = Offset(size.width, 0f),
+                            strokeWidth = strokeWidth,
+                        )
+                    },
         ) {
             Row(
                 modifier =
