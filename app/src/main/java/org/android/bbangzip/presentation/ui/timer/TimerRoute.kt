43ccb168f68ffb140493115cfa7e4c3317eceab9
kamja0510 <kamja990510@gmail.com>
2025-05-12 18:48:52 +0900

    [chore] #8 예시 코드 삭제

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/TimerRoute.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/TimerRoute.kt
@@ -1,31 +1,17 @@
 package org.android.bbangzip.presentation.ui.timer
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TimerRoute(modifier: Modifier = Modifier) {
-    Box(
-        modifier =
-            Modifier
-                .fillMaxSize()
-                .background(Color.Blue),
-    )
+    Text("timer")
+}
 
-    Column(
-        modifier =
-            modifier
-                .statusBarsPadding()
-                .fillMaxSize()
-                .background(Color.White),
-    ) {
-        Text("timer")
-    }
+@Preview(showSystemUi = true)
+@Composable
+private fun TimerRoutePreview() {
+    TimerRoute()
 }
