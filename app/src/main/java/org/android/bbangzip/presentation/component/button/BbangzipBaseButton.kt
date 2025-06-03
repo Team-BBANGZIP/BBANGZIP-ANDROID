38ed622918c3ca0fc23dcc9922623d6a63d96eff
kamja0510 <kamja990510@gmail.com>
2025-06-04 03:49:28 +0900

    [feat] #13 padding을 고정으로 두도록 변경하여 글씨 크기가 변해도 어색하지 않게 구현

--- app/src/main/java/org/android/bbangzip/presentation/component/button/BbangzipBaseButton.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/button/BbangzipBaseButton.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -54,7 +53,6 @@ fun BbangzipBaseButton(
             ),
     ) {
         Row(
-            modifier = Modifier,
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
         ) {
