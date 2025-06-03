3e423e5144701a2463a177a147b8b9c84804055f
kamja0510 <kamja990510@gmail.com>
2025-06-04 01:09:48 +0900

    [feat] #13 BbangZipBaseButton 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/component/button/BbangzipBaseButton.kt
@@ -0,0 +1,470 @@
+package org.android.bbangzip.presentation.component.button
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.android.bbangzip.R
+import org.android.bbangzip.presentation.util.extension.Gap
+import org.android.bbangzip.ui.theme.BBANGZIPANDROIDTheme
+import org.android.bbangzip.ui.theme.BbangZipTheme
+
+/**
+ * @param onClick 버튼 클릭 이벤트
+ */
+@Composable
+fun BbangzipBaseButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    containerColor: Color,
+    contentColor: Color,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    content: @Composable RowScope.() -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.height(48.dp),
+        shape = RoundedCornerShape(32.dp),
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor,
+            disabledContainerColor = BbangZipTheme.color.labelDisable_E4E2E0,
+            disabledContentColor = BbangZipTheme.color.labelAssistive_C9C7C5
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            leadingIcon?.let {
+                it()
+                Gap(width = 4)
+            }
+
+            content()
+
+            trailingIcon?.let {
+                Gap(width = 4)
+                it()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BbangzipBaseButtonPreview() {
+    BBANGZIPANDROIDTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+            ,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(140f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_plus_bold_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "30분 더",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(187f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_book_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "완료한 일 체크",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(140f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_plus_bold_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "60분 더",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(187f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_book_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "완료한 일 체크",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_go_back_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "돌아가기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_x_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "초기화 하기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_go_back_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "돌아가기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_x_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "종료 하기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(140f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_plus_bold_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "30분 더",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(187f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_x_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "종료하기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_x_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "취소",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_check_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "설정",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+
+            BbangzipBaseButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {},
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_trash_default_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                },
+                containerColor = BbangZipTheme.color.primaryLight_C8B5A2,
+                contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                content = {
+                    Text(
+                        text ="삭제하기",
+                        style = BbangZipTheme.typography.body2Medium
+                    )
+                }
+            )
+
+            BbangzipBaseButton(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 28.dp),
+                onClick = {},
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_calendar_default_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                },
+                containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                content = {
+                    Text(
+                        text = "저장하기",
+                        style = BbangZipTheme.typography.body2Medium
+                    )
+                }
+            )
+
+            BbangzipBaseButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {},
+                enabled = false,
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_trash_default_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                },
+                containerColor = BbangZipTheme.color.primaryLight_C8B5A2,
+                contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                content = {
+                    Text(
+                        text ="삭제하기",
+                        style = BbangZipTheme.typography.body2Medium
+                    )
+                }
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ){
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_return_thin_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(20.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryNormal_897869,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "돌아가기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+
+                Gap(width = 8)
+
+                BbangzipBaseButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_trash_default_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(20.dp)
+                        )
+                    },
+                    containerColor = BbangZipTheme.color.primaryStrong_4B4137,
+                    contentColor = BbangZipTheme.color.staticWhite_FFFFFF,
+                    content = {
+                        Text(
+                            text = "삭제하기",
+                            style = BbangZipTheme.typography.body2Medium
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
+
