b391a042ec9ebb140754d14c358a7ca4206f2eae
kamja0510 <kamja990510@gmail.com>
2025-06-05 04:06:25 +0900

    [fix] #15 상수명 오류 수정

--- app/src/main/java/org/android/bbangzip/presentation/component/toggle/BbangZipSwitch.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/toggle/BbangZipSwitch.kt
@@ -41,7 +41,7 @@ fun BbangZipSwitch(
     modifier: Modifier = Modifier,
     checkedThumbColor: Color = BbangZipTheme.color.primaryNormal_897869,
     uncheckedThumbColor: Color = BbangZipTheme.color.labelAssistive_C9C7C5,
-    switchPadding: Dp = ComponentConstants.Switch.DEFAULT_SWITCH_PADDING_DP.dp
+    switchPadding: Dp = ComponentConstants.Switch.DEFAULT_CONTAINER_PADDING_DP.dp
 ) {
     BoxWithConstraints(modifier = modifier) {
         val switchWidth = maxWidth
