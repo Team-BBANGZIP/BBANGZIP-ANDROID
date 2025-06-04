385233bdfea110a9ab5576f6086630a1620ce00d
kamja0510 <kamja990510@gmail.com>
2025-06-05 03:44:29 +0900

    [feat] #15 Modifier 확장함수인 dropShadow, innerShadow 구현

--- app/src/main/java/org/android/bbangzip/presentation/util/extension/ModiferExt.kt
+++ app/src/main/java/org/android/bbangzip/presentation/util/extension/ModiferExt.kt
@@ -1,5 +1,8 @@
 package org.android.bbangzip.presentation.util.extension
 
+import android.graphics.BlurMaskFilter
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -12,8 +15,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -64,3 +76,71 @@ fun Modifier.applyFilterOnClick(
                 onClick = { onClick() },
             )
     }
+
+fun Modifier.innerShadow(
+    shape: Shape,
+    color: Color,
+    blur: Dp,
+    offsetY: Dp,
+    offsetX: Dp,
+    spread: Dp
+) = drawWithContent {
+    drawContent()
+
+    val rect = Rect(Offset.Zero, size)
+    val paint = Paint().apply {
+        this.color = color
+        this.isAntiAlias = true
+    }
+
+    val shadowOutline = shape.createOutline(size, layoutDirection, this)
+
+    drawIntoCanvas { canvas ->
+        canvas.saveLayer(rect, paint)
+        canvas.drawOutline(shadowOutline, paint)
+
+        val frameworkPaint = paint.asFrameworkPaint()
+        frameworkPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+        if (blur.toPx() > 0) {
+            frameworkPaint.maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+        }
+        paint.color = Color.Black
+
+        val spreadOffsetX = offsetX.toPx() + if (offsetX.toPx() < 0) -spread.toPx() else spread.toPx()
+        val spreadOffsetY = offsetY.toPx() + if (offsetY.toPx() < 0) -spread.toPx() else spread.toPx()
+
+        canvas.translate(spreadOffsetX, spreadOffsetY)
+        canvas.drawOutline(shadowOutline, paint)
+        canvas.restore()
+    }
+
+}
+
+fun Modifier.dropShadow(
+    shape: Shape,
+    color: Color = Color.Black.copy(0.25f),
+    blur: Dp = 4.dp,
+    offsetY: Dp = 4.dp,
+    offsetX: Dp = 0.dp,
+    spread: Dp = 0.dp
+) = this.drawBehind {
+    val shadowSize = Size(size.width + spread.toPx(), size.height + spread.toPx())
+    val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
+
+    val paint = Paint().apply {
+        this.color = color
+    }
+
+    if (blur.toPx() > 0) {
+        paint.asFrameworkPaint().apply {
+            maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+        }
+    }
+
+    drawIntoCanvas { canvas ->
+        canvas.save()
+        canvas.translate(offsetX.toPx(), offsetY.toPx())
+        canvas.drawOutline(shadowOutline, paint)
+        canvas.restore()
+    }
+}
