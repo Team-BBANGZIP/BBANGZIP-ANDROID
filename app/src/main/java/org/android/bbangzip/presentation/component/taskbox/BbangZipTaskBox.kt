cd1d4c618808d6dbf9ee56afd0f046d577a79484
kamja0510 <kamja990510@gmail.com>
2025-06-09 06:15:18 +0900

    [feat] #21 BbangZipTaskBox.kt 생성

---
+++ app/src/main/java/org/android/bbangzip/presentation/component/taskbox/BbangZipTaskBox.kt
@@ -0,0 +1,20 @@
+package org.android.bbangzip.presentation.component.taskbox
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.android.bbangzip.ui.theme.BBANGZIPANDROIDTheme
+
+@Composable
+fun BbangZipTaskBox() {
+}
+
+@Preview
+@Composable
+fun BbangZipTaskBoxPreview() {
+    BBANGZIPANDROIDTheme {
+        Column {
+            BbangZipTaskBox()
+        }
+    }
+}
