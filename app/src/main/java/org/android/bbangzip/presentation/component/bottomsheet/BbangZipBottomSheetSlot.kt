5275599bd47e990235a48b9a99748c8034d23465
kamja0510 <kamja990510@gmail.com>
2025-06-08 22:22:20 +0900

    [chore] #19 상수화 및 컴포넌트화 완료

--- app/src/main/java/org/android/bbangzip/presentation/component/bottomsheet/BbangZipBottomSheetSlot.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/bottomsheet/BbangZipBottomSheetSlot.kt
@@ -1,16 +1,11 @@
 package org.android.bbangzip.presentation.component.bottomsheet
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -26,9 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import org.android.bbangzip.ui.theme.BBANGZIPANDROIDTheme
-import org.android.bbangzip.ui.theme.BbangZipTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,7 +30,8 @@ fun BbangZipBottomSheetSlot(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
     sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
-    contentPadding: PaddingValues = PaddingValues(top = 25.dp, start = 20.dp, end = 20.dp, bottom = 12.dp),
+    contentPadding: PaddingValues = BbangZipBottomSheetDefaults.CONTENT_PADDING,
+    dragHandle: @Composable () -> Unit = { BbangZipBottomSheetDefaults.DragHandle() },
     title: @Composable (ColumnScope.() -> Unit) = {},
     content: @Composable (ColumnScope.() -> Unit) = {},
     interactRow: @Composable (ColumnScope.() -> Unit) = {},
@@ -46,20 +40,9 @@ fun BbangZipBottomSheetSlot(
         ModalBottomSheet(
             onDismissRequest = onDismissRequest,
             sheetState = sheetState,
-            dragHandle = {
-                    Box(
-                        modifier = Modifier
-                            .padding(top = 6.dp)
-                            .width(60.dp)
-                            .height(5.dp)
-                            .background(
-                                color = BbangZipTheme.color.labelDisable_E4E2E0,
-                                shape = RoundedCornerShape(4.dp)
-                            )
-                    )
-                         },
-            shape = RoundedCornerShape(topStart = 48.dp, topEnd = 48.dp),
-            containerColor = BbangZipTheme.color.backgroundNormal_FFFFFF,
+            dragHandle = dragHandle,
+            shape = BbangZipBottomSheetDefaults.BOTTOM_SHEET_SHAPE,
+            containerColor = BbangZipBottomSheetDefaults.containerColor(),
         ) {
             Column(
                 modifier =
@@ -79,7 +62,7 @@ fun BbangZipBottomSheetSlot(
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-private fun BbangZipBottomSheetPreview() {
+private fun BbangZipBottomSheetSlotPreview() {
     BBANGZIPANDROIDTheme {
         var isBottomSheetVisible by remember { mutableStateOf(true) }
         var isBottomSheetVisible2 by remember { mutableStateOf(false) }
