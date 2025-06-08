53e9396d7d9f4828465c86e6ffc7a8c8109aee53
beom84 <cjja00@konkuk.ac.kr>
2025-06-08 16:06:51 +0900

    [feat] #11 add ComponentConstants.kt

---
+++ app/src/main/java/org/android/bbangzip/ui/theme/constant/ComponentConstants.kt
@@ -0,0 +1,12 @@
+package org.android.bbangzip.ui.theme.constant
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
+
+object ComponentConstants {
+    object TextField {
+        val DEFAULT_CONTENT_PADDING = PaddingValues(start = 12.dp, end = 12.dp, top = 12.dp, bottom = 10.dp)
+        const val DEFAULT_MAX_CHARACTER = 50
+        const val DEFAULT_BORDER_SIZE_DP = 0
+    }
+}
