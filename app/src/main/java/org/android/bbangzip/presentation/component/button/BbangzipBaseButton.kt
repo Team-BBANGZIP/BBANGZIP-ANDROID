12a1adb2753c2efa8c38911578db255b8f01dd38
kamja0510 <kamja990510@gmail.com>
2025-06-25 20:42:46 +0900

    [chore] #13 preview 삭제하기 버튼 클릭시 비활성화 디자인 볼 수 있도록 변경

--- app/src/main/java/org/android/bbangzip/presentation/component/button/BbangzipBaseButton.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/button/BbangzipBaseButton.kt
@@ -15,6 +15,10 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -90,6 +94,7 @@ fun BbangzipBaseButton(
 @Preview(showBackground = true)
 @Composable
 private fun BbangzipBaseButtonPreview() {
+    var isEnable by remember { mutableStateOf(true) }
     BBANGZIPANDROIDTheme {
         Column(
             modifier =
@@ -423,8 +428,8 @@ private fun BbangzipBaseButtonPreview() {
 
             BbangzipBaseButton(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = {},
-                enabled = false,
+                onClick = { isEnable = !isEnable },
+                enabled = isEnable,
                 leadingIcon = {
                     Icon(
                         painter = painterResource(R.drawable.ic_trash_default_24),
