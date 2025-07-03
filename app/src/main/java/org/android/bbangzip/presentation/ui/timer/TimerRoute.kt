446e79011e12db998414662179abf9bb5f3f9bed
beom84 <cjja00@konkuk.ac.kr>
2025-07-03 15:28:14 +0900

    [feat] #24 add TimerRoute.kt

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/TimerRoute.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/TimerRoute.kt
@@ -1,17 +1,60 @@
 package org.android.bbangzip.presentation.ui.timer
 
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
-fun TimerRoute(modifier: Modifier = Modifier) {
-    Text("timer")
+fun TimerRoute(
+    modifier: Modifier = Modifier,
+    showBottomBar: () -> Unit = {},
+    hideBottomBar: () -> Unit = {},
+    navigateToCompleteTask: () -> Unit = {},
+    timerViewmodel: TimerViewModel = hiltViewModel()
+) {
+    val timerState by timerViewmodel.uiState.collectAsStateWithLifecycle()
+    val success by timerViewmodel.success.collectAsStateWithLifecycle(initialValue = false)
+
+    LaunchedEffect(timerViewmodel.uiSideEffect) {
+        timerViewmodel.uiSideEffect.collect { effect ->
+            when (effect) {
+                TimerContract.TimerSideEffect.NavigateToCompleteTask -> navigateToCompleteTask()
+                TimerContract.TimerSideEffect.ShowBottomBar -> showBottomBar()
+                TimerContract.TimerSideEffect.HideBottomBar -> hideBottomBar()
+            }
+        }
+    }
+
+    when (success) {
+        true -> {
+            TimerScreen(
+                timerState = timerState,
+                modifier = modifier,
+                onBreadIconClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnBreadIconClick) },
+                onResetBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnResetBtnClick) },
+                onRepeatSheetApproveBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnRepeatSheetApproveBtnClick) },
+                onRepeatSheetDismissBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnRepeatSheetDismissBtnClick) },
+                onRepeatBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnRepeatBtnClick) },
+                onStartBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnStartBtnClick) },
+                onStopBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnStopBtnClick) },
+                onTimeOptionToggleClick = { timeOption -> timerViewmodel.setEvent(TimerContract.TimerEvent.OnTimeOptionToggleClick(timeOption)) },
+                onBreadSelectionSheetClick = { breadType -> timerViewmodel.setEvent(TimerContract.TimerEvent.OnBreadSelectionSheetClick(breadType = breadType)) },
+                onCompleteSheetCheckBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnCompleteSheetCheckBtnClick) },
+                onCompleteSheetRetryBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnCompleteSheetRetryBtnClick) },
+                onCompleteSheetDismissRequest = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnCompleteSheetDismissRequest) },
+                onEndConfirmSheetApproveBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnEndConfirmSheetApproveBtnClick) },
+                onEndConfirmSheetDismissBtnClick = { timerViewmodel.setEvent(TimerContract.TimerEvent.OnEndConfirmSheetDismissBtnClick) }
+
+            )
+        }
+
+        false -> {
+            CircularProgressIndicator()
+        }
+    }
 }
 
-@Preview(showSystemUi = true)
-@Composable
-private fun TimerRoutePreview() {
-    TimerRoute()
-}
