e0beeb1f4dd554d955053c791c0594c8da7ee71b
beom84 <cjja00@konkuk.ac.kr>
2025-07-23 16:25:08 +0900

    [feat] #24 add sharedViewModel,Contract

---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/shared/SharedContract.kt
@@ -0,0 +1,24 @@
+package org.android.bbangzip.presentation.ui.shared
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.android.bbangzip.presentation.util.base.BaseContract
+
+class SharedContract {
+    @Parcelize
+    data class SharedState(
+          val breadId : Int = 0,
+    ): BaseContract.State, Parcelable {
+        override fun toParcelable(): Parcelable = this
+    }
+
+    sealed class SharedEvent : BaseContract.Event {
+        data class OnClickBread(val breadId: Int) : SharedEvent()
+      }
+
+    sealed class SharedReduce :BaseContract.Reduce {
+        data class SetBreadId(val breadId: Int) : SharedReduce()
+    }
+
+    sealed class SharedSideEffect : BaseContract.SideEffect
+}
---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/shared/SharedViewModel.kt
@@ -0,0 +1,37 @@
+package org.android.bbangzip.presentation.ui.shared
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.android.bbangzip.presentation.util.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel
+@Inject
+constructor(
+savedStateHandle: SavedStateHandle
+) : BaseViewModel<SharedContract.SharedEvent, SharedContract.SharedState, SharedContract.SharedReduce, SharedContract.SharedSideEffect>(
+savedStateHandle = savedStateHandle
+) {
+
+    override fun createInitialState(savedState: Parcelable?): SharedContract.SharedState {
+        return SharedContract.SharedState()
+    }
+
+    override fun handleEvent(event: SharedContract.SharedEvent) {
+        when (event) {
+            is SharedContract.SharedEvent.OnClickBread -> {
+                updateState(SharedContract.SharedReduce.SetBreadId(event.breadId))
+            }
+        }
+    }
+
+    override fun reduceState(state: SharedContract.SharedState, reduce: SharedContract.SharedReduce): SharedContract.SharedState {
+        return when (reduce) {
+            is SharedContract.SharedReduce.SetBreadId -> {
+                state.copy(breadId = reduce.breadId)
+            }
+        }
+    }
+}
