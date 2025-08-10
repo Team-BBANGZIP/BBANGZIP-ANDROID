02f1bd1f144f8dad3db8f82a56b0514994424b32
beom84 <cjja00@konkuk.ac.kr>
2025-08-10 21:35:31 +0900

    [fix] #24 fix string, contentpadding

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/component/bottomsheet/TimerActionBottomSheet.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/component/bottomsheet/TimerActionBottomSheet.kt
@@ -1,12 +1,13 @@
 package org.android.bbangzip.presentation.ui.timer.component.bottomsheet
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -16,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import org.android.bbangzip.presentation.component.bottomsheet.BbangZipBottomSheetSlot
@@ -28,39 +28,37 @@ import org.android.bbangzip.ui.theme.BbangZipTheme
 @Composable
 fun TimerActionBottomSheet(
     isBottomSheetVisible: Boolean,
-    @StringRes titleText: Int,
-    @StringRes subTitleText: Int,
-    content: @Composable () -> Unit,
-    @StringRes leftBtnText: Int,
-    @StringRes rightBtnText: Int,
+    titleText: String,
+    subTitleText: String,
+    leftBtnText: String,
+    rightBtnText: String,
     @DrawableRes leftBtnIcon: Int,
     @DrawableRes rightBtnIcon: Int,
+    content: @Composable () -> Unit,
     onLeftClick: () -> Unit,
     onRightClick: () -> Unit,
     onDismissRequest: () -> Unit,
-    timeOptionIndex: Int,
     modifier: Modifier = Modifier,
 ) {
     BbangZipBottomSheetSlot(
         isBottomSheetVisible = isBottomSheetVisible,
         onDismissRequest = onDismissRequest,
         modifier = modifier,
-        contentPadding = contentPadding(top = 40.dp),
+        contentPadding = contentPadding(top = 40.dp, start = 0.dp, end = 0.dp, bottom = 0.dp),
         title = {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
-                    text = stringResource(titleText),
+                    text = titleText,
                     color = BbangZipTheme.color.primaryNormal_897869,
                     style = BbangZipTheme.typography.title1SemiBold,
                 )
 
                 Gap(4)
 
-
                 Text(
-                    text = stringResource(subTitleText),
+                    text = subTitleText,
                     color = BbangZipTheme.color.labelAlternative_A29D96,
                     style = BbangZipTheme.typography.body1Medium,
                 )
@@ -77,51 +75,62 @@ fun TimerActionBottomSheet(
             Row(
                 modifier =
                 Modifier
+                    .fillMaxWidth()
                     .padding(horizontal = 16.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 Row(
                     modifier = Modifier
+                        .fillMaxWidth()
                         .background(color = BbangZipTheme.color.primaryNormal_897869, shape = RoundedCornerShape(size = 32.dp))
-                        .noRippleClickable { onLeftClick() },
+                        .noRippleClickable { onLeftClick() }
+                        .padding(vertical = 14.dp)
+                        .weight(1f),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Text(
-                        text = stringResource(leftBtnText),
-                        color = BbangZipTheme.color.labelAlternative_A29D96,
-                        style = BbangZipTheme.typography.subTitle1Medium,
-                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                        text = leftBtnText,
+                        color = BbangZipTheme.color.staticWhite_FFFFFF,
+                        style = BbangZipTheme.typography.body2Medium,
+                        modifier = Modifier,
                     )
+
+                    Gap(4)
 
                     Icon(
                         imageVector = ImageVector.vectorResource(id = leftBtnIcon),
                         contentDescription = null,
-                        tint = BbangZipTheme.color.primaryNormal_897869,
-                        modifier = Modifier.padding(end = 8.dp)
+                        tint = BbangZipTheme.color.staticWhite_FFFFFF,
+                        modifier = Modifier.size(16.dp)
                     )
                 }
 
                 Row(
                     modifier = Modifier
+                        .fillMaxWidth()
                         .background(color = BbangZipTheme.color.primaryStrong_4B4137, shape = RoundedCornerShape(size = 32.dp))
-                        .noRippleClickable { onRightClick() },
+                        .noRippleClickable { onRightClick() }
+                        .padding(vertical = 16.dp)
+                        .weight(1f),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Text(
-                        text = stringResource(rightBtnText),
-                        color = BbangZipTheme.color.labelAlternative_A29D96,
-                        style = BbangZipTheme.typography.subTitle1Medium,
-                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                        text = rightBtnText,
+                        color = BbangZipTheme.color.staticWhite_FFFFFF,
+                        style = BbangZipTheme.typography.body2Medium,
+                        modifier = Modifier,
                     )
+
+                    Gap(4)
 
                     Icon(
                         imageVector = ImageVector.vectorResource(id = rightBtnIcon),
                         contentDescription = null,
-                        tint = BbangZipTheme.color.primaryNormal_897869,
-                        modifier = Modifier.padding(end = 8.dp)
+                        tint = BbangZipTheme.color.staticWhite_FFFFFF,
+                        modifier = Modifier.size(16.dp)
                     )
                 }
             }
