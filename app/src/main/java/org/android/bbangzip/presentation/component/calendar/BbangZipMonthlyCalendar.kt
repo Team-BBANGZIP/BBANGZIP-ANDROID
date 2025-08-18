4c10aeb5c2b8ec585eb98e10b926d4b7c42a0d89
kamja0510 <kamja990510@gmail.com>
2025-08-18 19:37:41 +0900

    [refactor] #26 preview 수정

--- app/src/main/java/org/android/bbangzip/presentation/component/calendar/BbangZipMonthlyCalendar.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/calendar/BbangZipMonthlyCalendar.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -30,6 +32,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.android.bbangzip.R
 import org.android.bbangzip.presentation.util.extension.Gap
 import org.android.bbangzip.presentation.util.extension.noRippleClickable
@@ -373,8 +376,16 @@ private fun getDaysOfWeekStartingFrom(startDayOfWeek: DayOfWeek = DayOfWeek.MOND
 @Composable
 fun MonthlyCalendarPreview() {
     BBANGZIPANDROIDTheme {
-        MonthlyCalendar(onDateSelected = {
-            Timber.tag("BbangZipMonthlyCalendar").d("onDateSelected: $it")
-        })
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            MonthlyCalendar(
+                onDateSelected = {
+                    Timber.tag("BbangZipMonthlyCalendar").d("onDateSelected: $it")
+                }
+            )
+        }
     }
 }
