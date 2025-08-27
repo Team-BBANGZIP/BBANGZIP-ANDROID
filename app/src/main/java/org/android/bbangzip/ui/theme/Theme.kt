a8a59b88d8d7ecb76f3ed93a2f46a2c9fa0db5b7
kamja0510 <kamja990510@gmail.com>
2025-08-28 02:16:46 +0900

    [feat] #30 상태 표시줄 아이콘 색상 어둡게 변경

--- app/src/main/java/org/android/bbangzip/ui/theme/Theme.kt
+++ app/src/main/java/org/android/bbangzip/ui/theme/Theme.kt
@@ -1,13 +1,29 @@
 package org.android.bbangzip.ui.theme
 
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
 fun BBANGZIPANDROIDTheme(
     content: @Composable () -> Unit,
 ) {
+    // 상태 표시줄 아이콘 색상 어둡게 변경
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+
+            WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
+            WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightNavigationBars = true
+        }
+    }
+
     val bbangZipTypography = defaultBbangZipTypography
     val bbangZipColor = defaultBbangZipColor
     val bbangZipOpacity = defaultBbangZipOpacity
