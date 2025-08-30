e91f9eff4f02de513a5561e9b09520820758ca78
kamja0510 <kamja990510@gmail.com>
2025-08-30 14:27:02 +0900

    [chore] #30 CategoryColor enum class 생성

---
+++ app/src/main/java/org/android/bbangzip/presentation/model/CategoryColor.kt
@@ -0,0 +1,14 @@
+package org.android.bbangzip.presentation.model
+
+enum class CategoryColor(val colorName: String) {
+    TODO_RED_1("todoRed1_EA7152"),
+    TODO_RED_2("todoRed2_F09C86"),
+    TODO_YELLOW_1("todoYellow1_FED45C"),
+    TODO_YELLOW_2("todoYellow2_F6DDAF"),
+    TODO_GREEN_1("todoGreen1_7A946D"),
+    TODO_GREEN_2("todoGreen2_A2B499"),
+    TODO_BLUE_1("todoBlue1_5C62AC"),
+    TODO_BLUE_2("todoBlue2_8D91C5"),
+    TODO_PURPLE_1("todoPurple1_8F63E9"),
+    TODO_PURPLE_2("todoPurple2_B79FE8"),
+}
