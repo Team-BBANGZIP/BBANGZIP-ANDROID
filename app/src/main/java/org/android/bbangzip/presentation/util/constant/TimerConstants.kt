161addc94f10bbe25d802f98beac28b39733e35c
beom84 <cjja00@konkuk.ac.kr>
2025-08-30 18:08:11 +0900

    [chore] #24 background reset 상수 선언

--- app/src/main/java/org/android/bbangzip/presentation/util/constant/TimerConstants.kt
+++ app/src/main/java/org/android/bbangzip/presentation/util/constant/TimerConstants.kt
@@ -3,4 +3,5 @@ package org.android.bbangzip.presentation.util.constant
 object TimerConstants {
     const val THIRTY_MINUTES = 1800000L
     const val SIXTY_MINUTES = 3600000L
+    const val BACKGROUND_RESET_THRESHOLD = 60000L
 }
