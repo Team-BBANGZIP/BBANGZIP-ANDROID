cfddd31b79c647f7edf2d06a3306bb80bf8d6d82
beom84 <cjja00@konkuk.ac.kr>
2025-09-02 11:42:49 +0900

    [chore] #24 exit 애니메이션 삭제

--- app/src/main/java/org/android/bbangzip/presentation/ui/navigator/component/BottomNavigationBar.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/navigator/component/BottomNavigationBar.kt
@@ -4,7 +4,6 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -49,25 +48,25 @@ fun BottomNavigationBar(
     ) {
         Box(
             modifier =
-                modifier
-                    .fillMaxWidth()
-                    .background(color = BbangZipTheme.color.componentIvory_FDFDFD)
-                    .drawBehind {
-                        val strokeWidth = 1.dp.toPx()
-                        drawLine(
-                            color = borderColor,
-                            start = Offset(0f, 0f),
-                            end = Offset(size.width, 0f),
-                            strokeWidth = strokeWidth,
-                        )
-                    }
-                    .padding(top = 10.dp),
+            modifier
+                .fillMaxWidth()
+                .background(color = BbangZipTheme.color.componentIvory_FDFDFD)
+                .drawBehind {
+                    val strokeWidth = 1.dp.toPx()
+                    drawLine(
+                        color = borderColor,
+                        start = Offset(0f, 0f),
+                        end = Offset(size.width, 0f),
+                        strokeWidth = strokeWidth,
+                    )
+                }
+                .padding(top = 10.dp),
         ) {
             Row(
                 modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(top = 4.dp, start = 16.dp, end = 16.dp, bottom = 8.dp),
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp, start = 16.dp, end = 16.dp, bottom = 8.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
             ) {
@@ -97,10 +96,10 @@ private fun BottomNavigationItem(
 ) {
     Column(
         modifier =
-            modifier
-                .noRippleClickable {
-                    onBottomNaviBarItemSelected(bottomNaviType)
-                },
+        modifier
+            .noRippleClickable {
+                onBottomNaviBarItemSelected(bottomNaviType)
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Box(
@@ -111,11 +110,11 @@ private fun BottomNavigationItem(
                 contentDescription = stringResource(bottomNaviTitle),
                 modifier = Modifier.size(20.dp),
                 tint =
-                    if (isSelected) {
-                        BbangZipTheme.color.labelStrong_463D34
-                    } else {
-                        BbangZipTheme.color.labelAssistive_C9C7C5
-                    },
+                if (isSelected) {
+                    BbangZipTheme.color.labelStrong_463D34
+                } else {
+                    BbangZipTheme.color.labelAssistive_C9C7C5
+                },
             )
         }
 
@@ -124,17 +123,17 @@ private fun BottomNavigationItem(
         Text(
             text = stringResource(bottomNaviTitle),
             color =
-                if (isSelected) {
-                    BbangZipTheme.color.labelStrong_463D34
-                } else {
-                    BbangZipTheme.color.labelAssistive_C9C7C5
-                },
+            if (isSelected) {
+                BbangZipTheme.color.labelStrong_463D34
+            } else {
+                BbangZipTheme.color.labelAssistive_C9C7C5
+            },
             style =
-                if (isSelected) {
-                    BbangZipTheme.typography.label5SemiBold
-                } else {
-                    BbangZipTheme.typography.label6Medium
-                },
+            if (isSelected) {
+                BbangZipTheme.typography.label5SemiBold
+            } else {
+                BbangZipTheme.typography.label6Medium
+            },
         )
     }
 }
