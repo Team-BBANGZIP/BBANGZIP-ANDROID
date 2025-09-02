9621285090815213e08c9610c6b1d8477f27b954
beom84 <cjja00@konkuk.ac.kr>
2025-09-02 14:21:19 +0900

    [feat] #24 ktlint

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/component/bottomsheet/BreadSelectBottomSheet.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/component/bottomsheet/BreadSelectBottomSheet.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.android.bbangzip.R
 import org.android.bbangzip.presentation.component.bottomsheet.BbangZipBottomSheetSlot
