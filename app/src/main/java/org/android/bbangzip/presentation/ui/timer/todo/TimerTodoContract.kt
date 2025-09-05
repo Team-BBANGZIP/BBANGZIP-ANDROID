d23482f7bf83704d3a5ce06b9a334f997d0b6cfb
beom84 <cjja00@konkuk.ac.kr>
2025-09-05 15:31:15 +0900

    [feat] #32 timer contract 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/todo/TimerTodoContract.kt
@@ -0,0 +1,45 @@
+package org.android.bbangzip.presentation.ui.timer.todo
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.android.bbangzip.presentation.model.Category
+import org.android.bbangzip.presentation.model.ListItem
+import org.android.bbangzip.presentation.util.base.BaseContract
+import java.time.LocalDate
+
+class TimerTodoContract {
+    @Parcelize
+    data class TimerTodoState(
+        val categories: List<Category> = emptyList(),
+        val flatList: List<ListItem> = emptyList(),
+        val todayDate: LocalDate = LocalDate.now(),
+        val isAddTodoBottomSheetVisible: Boolean = false,
+        val isTimePickerBottomSheetVisible: Boolean = false,
+    ) : BaseContract.State, Parcelable {
+        override fun toParcelable(): Parcelable = this
+    }
+
+    sealed interface TimerTodoEvent : BaseContract.Event {
+        data object Initialize : TimerTodoEvent
+
+        data object OnMenuClick : TimerTodoEvent
+
+        data class OnTodoCheckBoxClick(val todoId: Int, val categoryId: Int, val isChecked: Boolean) : TimerTodoEvent
+
+        data object OnRestartTimerBtnClick : TimerTodoEvent
+
+        data object OnExitBtnClick : TimerTodoEvent
+
+        data object OnBackIconClick : TimerTodoEvent
+    }
+
+    sealed interface TimerTodoReduce : BaseContract.Reduce {
+        data class UpdateFlatList(val flatList: List<ListItem>) : TimerTodoReduce
+    }
+
+    sealed interface TimerTodoSideEffect : BaseContract.SideEffect {
+        data class NavigateToTimer(val shouldRestart: Boolean) : TimerTodoSideEffect
+
+        data object NavigateToBack : TimerTodoSideEffect
+    }
+}
