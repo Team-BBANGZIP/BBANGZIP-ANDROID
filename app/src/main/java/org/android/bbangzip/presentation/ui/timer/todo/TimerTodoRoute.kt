0cfd3db0dc00bb64c09cc9b3bba44876947c3c14
beom84 <cjja00@konkuk.ac.kr>
2025-09-05 16:30:07 +0900

    [feat] #32 timertodo Route 구현

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/todo/TimerTodoRoute.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/todo/TimerTodoRoute.kt
@@ -1,13 +1,40 @@
 package org.android.bbangzip.presentation.ui.timer.todo
 
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun TimerTodoRoute(
-    navigateToTimer: () -> Unit, modifier: Modifier = Modifier
+    navigateToTimer: (shouldRestart : Boolean) -> Unit,
+    navigateToBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: TimerTodoViewModel = hiltViewModel(),
 ) {
-    TimerTodoScreen(
-        modifier = modifier,
-    )
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val success by viewModel.success.collectAsStateWithLifecycle(initialValue = true)
+
+    LaunchedEffect(viewModel.uiSideEffect) {
+        viewModel.uiSideEffect.collectLatest { effect ->
+            when (effect) {
+                is TimerTodoContract.TimerTodoSideEffect.NavigateToBack -> navigateToBack()
+                is TimerTodoContract.TimerTodoSideEffect.NavigateToTimer -> navigateToTimer(effect.shouldRestart)
+            }
+        }
+    }
+
+    when(success) {
+        true -> TimerTodoScreen(
+            modifier = modifier,
+        )
+        false -> {
+            CircularProgressIndicator()
+        }
+    }
+
 }
