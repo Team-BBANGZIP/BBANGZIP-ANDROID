26404ae44d40782c78e503397801a3dd54ad801d
beom84 <cjja00@konkuk.ac.kr>
2025-09-07 15:26:47 +0900

    [feat] #38 CompleteEntity entity 구현

--- app/src/main/java/org/android/bbangzip/data/dto/response/ResponseCompleteTimerDto.kt
+++ app/src/main/java/org/android/bbangzip/data/dto/response/ResponseCompleteTimerDto.kt
@@ -2,9 +2,15 @@ package org.android.bbangzip.data.dto.response
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.android.bbangzip.domain.model.CompleteBreadCountEntity
 
 @Serializable
 data class ResponseCompleteTimerDto(
     @SerialName("count")
     val count: Int,
-)
+){
+    fun toCompleteTimerEntity() =
+        CompleteBreadCountEntity(
+            count = count,
+        )
+}
