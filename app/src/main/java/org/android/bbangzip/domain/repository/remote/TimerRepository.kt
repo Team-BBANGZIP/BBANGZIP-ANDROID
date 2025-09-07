8b356158904f3c96b49d650b3bf32a6c7d8f3d9a
beom84 <cjja00@konkuk.ac.kr>
2025-09-07 15:29:18 +0900

    [feat] #38 TimerRepository.kt 구현

---
+++ app/src/main/java/org/android/bbangzip/domain/repository/remote/TimerRepository.kt
@@ -0,0 +1,11 @@
+package org.android.bbangzip.domain.repository.remote
+
+import org.android.bbangzip.domain.model.BreadListEntity
+import org.android.bbangzip.domain.model.CompleteBreadCountEntity
+import org.android.bbangzip.domain.model.TodayBreadCountEntity
+
+interface TimerRepository {
+    suspend fun postTimerCompleted(): Result<CompleteBreadCountEntity>
+    suspend fun fetchTodayBreadCount(): Result<TodayBreadCountEntity>
+    suspend fun fetchBreadList(): Result<BreadListEntity>
+}
