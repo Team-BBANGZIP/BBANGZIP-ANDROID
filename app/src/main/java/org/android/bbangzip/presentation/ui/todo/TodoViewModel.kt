776af6ebf7d693a2792c074ff54cb57824c8710c
kamja0510 <kamja990510@gmail.com>
2025-09-11 07:45:35 +0900

    [chore] #39 ktlint

--- app/src/main/java/org/android/bbangzip/presentation/ui/todo/TodoViewModel.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/todo/TodoViewModel.kt
@@ -102,10 +102,9 @@ class TodoViewModel
                     updateState(TodoReduce.UpdateIsAddTodoBottomSheetVisible(true))
                 }
                 TodoEvent.OnAddTodoBottomSheetDismissRequest -> {
-                    if (currentUiState.selectedCategory != null && currentUiState.todoText.isNotBlank())
-                        {
-                            onTodoAdd(categoryId = currentUiState.selectedCategory!!.categoryId, todoContent = currentUiState.todoText, startTime = currentUiState.selectedStartTime)
-                        }
+                    if (currentUiState.selectedCategory != null && currentUiState.todoText.isNotBlank()) {
+                        onTodoAdd(categoryId = currentUiState.selectedCategory!!.categoryId, todoContent = currentUiState.todoText, startTime = currentUiState.selectedStartTime)
+                    }
                     updateState(TodoReduce.ClearAddTodoState)
                     updateState(TodoReduce.UpdateIsAddTodoBottomSheetVisible(false))
                 }
