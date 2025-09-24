5965ef57b5766c40a1883646f7894a28804c2be1
JIEUNI <jieundaeun@naver.com>
2025-09-24 17:15:47 +0900

    [feat] #41 RequestUserInfoDto 구현

---
+++ app/src/main/java/org/android/bbangzip/data/dto/request/RequestUserInfoDto.kt
@@ -0,0 +1,22 @@
+package org.android.bbangzip.data.dto.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestUserInfoDto(
+    @SerialName("provider")
+    val provider: String,
+    @SerialName("role")
+    val role: String,
+    @SerialName("deviceName")
+    val deviceName: String,
+    @SerialName("deviceType")
+    val deviceType: String,
+    @SerialName("osType")
+    val osType: String,
+    @SerialName("osVersion")
+    val osVersion: String,
+    @SerialName("appVersion")
+    val appVersion: String
+)
