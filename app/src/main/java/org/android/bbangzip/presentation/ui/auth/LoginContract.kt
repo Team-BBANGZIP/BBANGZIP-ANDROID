d2c5ab959b826573a7b8154ce88e59a7bc249d35
JIEUNI <jieundaeun@naver.com>
2025-09-24 17:22:50 +0900

    [feat] #41 LoginContract 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/auth/LoginContract.kt
@@ -0,0 +1,36 @@
+package org.android.bbangzip.presentation.ui.auth
+
+import android.content.Context
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.android.bbangzip.presentation.util.base.BaseContract
+
+class LoginContract {
+    @Parcelize
+    data class LoginState(
+        val isSloganVisible: Boolean = false,
+        val isBackgroundVisible: Boolean = false,
+        val isKakaoLoginBtnVisible: Boolean = false,
+        val isLoading: Boolean = false,
+        val isOnboardingCompleted: Boolean = false,
+        val loginState: Boolean = false,
+    ) : BaseContract.State, Parcelable {
+        override fun toParcelable(): Parcelable = this
+    }
+
+    sealed interface LoginEvent : BaseContract.Event {
+        data class OnClickKakaoLoginBtn(val context: Context) : LoginEvent
+    }
+
+    sealed interface LoginReduce : BaseContract.Reduce {
+        data class UpdateState(val state: LoginState) : LoginReduce
+
+        data class UpdateLoginSuccess(val success: Boolean) : LoginReduce
+    }
+
+    sealed interface LoginSideEffect : BaseContract.SideEffect {
+        data object NavigateToOnboarding : LoginSideEffect
+
+        data object NavigateToTodo : LoginSideEffect
+    }
+}
