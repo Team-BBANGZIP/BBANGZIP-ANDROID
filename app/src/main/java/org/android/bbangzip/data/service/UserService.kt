ba788a83082412ac6727a1a68d6cafc6c729d3fa
JIEUNI <jieundaeun@naver.com>
2025-09-24 17:21:31 +0900

    [feat] #41 UserService 구현

---
+++ app/src/main/java/org/android/bbangzip/data/service/UserService.kt
@@ -0,0 +1,44 @@
+package org.android.bbangzip.data.service
+
+import org.android.bbangzip.data.dto.request.RequestOnboardingDto
+import org.android.bbangzip.data.dto.request.RequestUserInfoDto
+import org.android.bbangzip.data.dto.response.ResponseReissueDto
+import org.android.bbangzip.data.dto.response.ResponseUserDto
+import org.android.bbangzip.data.util.base.BaseResponse
+import org.android.bbangzip.data.util.constant.ApiConstants.API
+import org.android.bbangzip.data.util.constant.ApiConstants.AUTH
+import org.android.bbangzip.data.util.constant.ApiConstants.CODE
+import org.android.bbangzip.data.util.constant.ApiConstants.REISSUE
+import org.android.bbangzip.data.util.constant.ApiConstants.SIGN_IN
+import org.android.bbangzip.data.util.constant.ApiConstants.SIGN_OUT
+import org.android.bbangzip.data.util.constant.ApiConstants.SIGN_UP
+import org.android.bbangzip.data.util.constant.ApiConstants.USER
+import org.android.bbangzip.data.util.constant.ApiConstants.VERSIONS
+import org.android.bbangzip.data.util.constant.ApiConstants.WITHDRAW
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface UserService {
+    @POST("$API/$VERSIONS/$USER/$AUTH/$SIGN_IN")
+    suspend fun login(
+        @Query(CODE) code: String,
+        @Body requestUserInfoDto: RequestUserInfoDto
+    ): BaseResponse<ResponseUserDto>
+
+    @POST("$API/$VERSIONS/$USER/$AUTH/$REISSUE")
+    suspend fun reissue(): BaseResponse<ResponseReissueDto>
+
+    @DELETE("$API/$VERSIONS/$USER/$AUTH/$SIGN_OUT")
+    suspend fun logout(): BaseResponse<String>
+
+    @DELETE("$API/$VERSIONS/$USER/$AUTH/$WITHDRAW")
+    suspend fun withdraw(): BaseResponse<String>
+
+    @PATCH("$API/$VERSIONS/$USER/$AUTH/$SIGN_UP")
+    suspend fun onboardingComplete(
+        @Body requestOnboardingDto: RequestOnboardingDto,
+    ): BaseResponse<String>
+}
