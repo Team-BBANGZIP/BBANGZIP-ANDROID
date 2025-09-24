34c68b7c62e7b9702abb2208b2b6a20232b88097
JIEUNI <jieundaeun@naver.com>
2025-09-24 17:20:06 +0900

    [add] #41 Auth관련 API 구현을 위한 상수 추가

--- app/src/main/java/org/android/bbangzip/data/util/constant/ApiConstants.kt
+++ app/src/main/java/org/android/bbangzip/data/util/constant/ApiConstants.kt
@@ -6,5 +6,15 @@ object ApiConstants {
 
     const val VERSIONS = "v1"
 
+    // User
+    const val USER = "user"
+    const val AUTH = "auth"
+    const val SIGN_IN = "signin"
+    const val REISSUE = "re-issue"
+    const val SIGN_OUT = "siginout"
+    const val WITHDRAW = "withdraw"
+    const val SIGN_UP = "signup"
+    const val CODE = "code"
+
     const val TIMER = "timers"
 }
