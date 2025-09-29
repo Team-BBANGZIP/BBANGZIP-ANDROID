58260da28b041894f4df752a91a111f1ddd1e74e
beom84 <cjja00@konkuk.ac.kr>
2025-09-30 03:05:53 +0900

    [refactor] #50 ktlint

--- app/src/main/java/org/android/bbangzip/data/repository/di/RepositoryModule.kt
+++ app/src/main/java/org/android/bbangzip/data/repository/di/RepositoryModule.kt
@@ -7,9 +7,9 @@ import dagger.hilt.components.SingletonComponent
 import org.android.bbangzip.data.repository.local.UserRepositoryImpl
 import org.android.bbangzip.data.repository.remote.DummyRepositoryImpl
 import org.android.bbangzip.data.repository.remote.TimerRepositoryImpl
-import org.android.bbangzip.domain.repository.UserRepository
 import org.android.bbangzip.domain.repository.DummyRepository
 import org.android.bbangzip.domain.repository.TimerRepository
+import org.android.bbangzip.domain.repository.UserRepository
 import javax.inject.Singleton
 
 @Module
