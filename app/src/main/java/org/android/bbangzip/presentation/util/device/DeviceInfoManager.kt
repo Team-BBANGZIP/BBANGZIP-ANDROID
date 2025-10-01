011b2418bdc32b130061da43e82f59a7d3ea6bd2
JIEUNI <jieundaeun@naver.com>
2025-10-02 00:19:31 +0900

    [feat] #41 DeviceManager 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/util/device/DeviceInfoManager.kt
@@ -0,0 +1,29 @@
+package org.android.bbangzip.presentation.util.device
+
+import android.os.Build
+import org.android.bbangzip.BuildConfig
+
+object DeviceInfoManager {
+    fun getDeviceInfo(): DeviceInfo {
+        val osVersion = Build.VERSION.RELEASE
+        val deviceName = Build.MODEL
+        val osType = "Android"
+        val appVersion = BuildConfig.VERSION_NAME
+
+        return DeviceInfo(
+            deviceName = deviceName,
+            deviceType = deviceName,
+            osVersion = osVersion,
+            osType = osType,
+            appVersion = appVersion
+        )
+    }
+}
+
+data class DeviceInfo(
+    val deviceName: String,
+    val deviceType: String,
+    val osVersion: String,
+    val osType: String,
+    val appVersion: String
+)
