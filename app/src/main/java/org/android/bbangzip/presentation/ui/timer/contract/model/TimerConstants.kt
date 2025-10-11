c872db253323199265156e2901d0e79d74fe5291
beom84 <cjja00@konkuk.ac.kr>
2025-10-12 00:40:03 +0900

    [feat] #50 사용하지 않는 type 삭제

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/contract/model/TimerConstants.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/contract/model/TimerConstants.kt
@@ -3,5 +3,4 @@ package org.android.bbangzip.presentation.ui.timer.contract.model
 object TimerConstants {
     const val THIRTY_MINUTES = 1800000L
     const val SIXTY_MINUTES = 3600000L
-    const val BACKGROUND_RESET_THRESHOLD = 60000L
 }
