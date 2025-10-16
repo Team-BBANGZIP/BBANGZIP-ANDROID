fa03e3fc6ecb93f42f7660a9b679777bae4b07f8
beom84 <cjja00@konkuk.ac.kr>
2025-10-16 20:07:26 +0900

    [fix] #50 BaseResponse message param 삭제

--- app/src/main/java/org/android/bbangzip/data/datasource/remote/util/base/BaseResponse.kt
+++ app/src/main/java/org/android/bbangzip/data/datasource/remote/util/base/BaseResponse.kt
@@ -7,8 +7,7 @@ import kotlinx.serialization.Serializable
 data class BaseResponse<T>(
     @SerialName("code")
     val code: Int,
-    @SerialName("message")
-    val message: String,
+
     @SerialName("data")
     val data: T,
 )
