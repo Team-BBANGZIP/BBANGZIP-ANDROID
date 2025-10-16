3ad78ec000561bb546ac8fe5011d434c85721fa7
beom84 <cjja00@konkuk.ac.kr>
2025-10-16 20:21:58 +0900

    [feat] #50 constant util로 이동

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/util/TimerConstants.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/util/TimerConstants.kt
@@ -1,4 +1,4 @@
-package org.android.bbangzip.presentation.ui.timer.contract.model
+package org.android.bbangzip.presentation.ui.timer.util
 
 object TimerConstants {
     const val THIRTY_MINUTES = 1800000L
