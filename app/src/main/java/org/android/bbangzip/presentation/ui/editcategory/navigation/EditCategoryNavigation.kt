3276cb6f7386b26690125c8dac9d9897bb998916
kamja0510 <kamja990510@gmail.com>
2025-10-18 17:40:37 +0900

    [feat] #55 CategoryNavType 구현

--- app/src/main/java/org/android/bbangzip/presentation/ui/editcategory/navigation/EditCategoryNavigation.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/editcategory/navigation/EditCategoryNavigation.kt
@@ -1,9 +1,13 @@
 package org.android.bbangzip.presentation.ui.editcategory.navigation
 
+import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import kotlinx.serialization.json.Json
+import org.android.bbangzip.presentation.common.model.Category
 import org.android.bbangzip.presentation.ui.editcategory.EditCategoryRoute
 
 fun NavController.navigateToEditCategory() {
@@ -20,4 +24,23 @@ fun NavGraphBuilder.editCategoryNavGraph(
             popBackStack = popBackStack,
         )
     }
+}
+
+val CategoryNavType =
+    object : NavType<Category>(isNullableAllowed = false) {
+        override fun get(bundle: Bundle, key: String): Category? {
+            return bundle.getString(key)?.let { Json.decodeFromString(it)}
+        }
+
+        override fun parseValue(value: String): Category {
+            return Json.decodeFromString(value)
+        }
+
+        override fun put(bundle: Bundle, key: String, value: Category) {
+            bundle.putString(key, Json.encodeToString(Category.serializer(), value))
+        }
+
+        override fun serializeAsValue(value: Category): String {
+            return Json.encodeToString(Category.serializer(), value)
+        }
 }
