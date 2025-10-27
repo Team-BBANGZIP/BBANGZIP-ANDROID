5e319bbef4e27199305da5f7d11927ab6ff80b2f
JIEUNI <jieundaeun@naver.com>
2025-10-28 00:30:25 +0900

    [feat] #41 Login Navigation 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/auth/LoginNavigation.kt
@@ -0,0 +1,30 @@
+package org.android.bbangzip.presentation.ui.auth
+
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToLogin() {
+    navigate(
+        route = LoginRoute,
+    ) {
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.loginNavGraph(
+    navigateToTodo: () -> Unit = {},
+    navigateToOnboarding: () -> Unit = {},
+) {
+    composable<LoginRoute> {
+        LoginRoute(
+            navigateToTodo = navigateToTodo,
+            navigateToOnboarding = navigateToOnboarding,
+        )
+    }
+}
+
+@Serializable
+object LoginRoute
---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/auth/LoginRoute.kt
@@ -0,0 +1,36 @@
+package org.android.bbangzip.presentation.ui.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+fun LoginRoute(
+    navigateToTodo: () -> Unit,
+    navigateToOnboarding: () -> Unit,
+    viewModel: LoginViewModel = hiltViewModel(),
+) {
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    LaunchedEffect(viewModel.uiSideEffect) {
+        viewModel.uiSideEffect.collectLatest { effect ->
+            when (effect) {
+                is LoginContract.LoginSideEffect.NavigateToTodo -> {
+                    navigateToTodo()
+                }
+
+                is LoginContract.LoginSideEffect.NavigateToOnboarding -> navigateToOnboarding()
+            }
+        }
+    }
+
+    LoginScreen(
+        state = state,
+        onClickKakaoLoginBtn = { viewModel.setEvent(LoginContract.LoginEvent.OnClickKakaoLoginBtn(context)) },
+    )
+}
