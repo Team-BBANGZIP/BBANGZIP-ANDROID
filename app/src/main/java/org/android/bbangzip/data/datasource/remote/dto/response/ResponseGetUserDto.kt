d148bac5e02ac5f243e0e930a3a06a3aa181971f
JIEUNI <jieundaeun@naver.com>
2025-10-27 22:15:42 +0900

    [feat] #41 ResponseGetUserDto 구현

---
+++ app/src/main/java/org/android/bbangzip/data/datasource/remote/dto/response/ResponseGetUserDto.kt
@@ -0,0 +1,23 @@
+package org.android.bbangzip.data.datasource.remote.dto.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.android.bbangzip.domain.model.UserEntity
+
+@Serializable
+data class ResponseGetUserDto(
+    @SerialName("accessToken")
+    val accessToken: String,
+    @SerialName("refreshToken")
+    val refreshToken: String,
+    @SerialName("isOnboardingComplete")
+    val isSignUpComplete: Boolean,
+) {
+    fun toUserEntity() =
+        UserEntity(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+            isSignUpComplete = isSignUpComplete,
+        )
+}
