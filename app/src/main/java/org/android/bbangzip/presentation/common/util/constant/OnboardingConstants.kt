6e06f2d021c95c5631e44ac5d280f14dd3792945
JIEUNI <jieundaeun@naver.com>
2025-11-06 17:01:46 +0900

    [feat] #64 온보딩Constants 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/common/util/constant/OnboardingConstants.kt
@@ -0,0 +1,29 @@
+package org.android.bbangzip.presentation.common.util.constant
+
+import androidx.annotation.DrawableRes
+import org.android.bbangzip.R
+import org.android.bbangzip.ui.theme.defaultBbangZipColor
+
+object OnboardingConstants {
+
+    @DrawableRes
+    val PROFILE_IMG_RES_IDS = listOf(
+        R.drawable.ic_profile_illust1_100,
+        R.drawable.ic_profile_illust2_100,
+        R.drawable.ic_profile_illust3_100,
+        R.drawable.ic_profile_illust4_100,
+        R.drawable.ic_profile_illust5_100,
+        R.drawable.ic_profile_illust6_100
+    )
+
+    val PROFILE_IMG_OUTLINE_COLORS = mapOf(
+        R.drawable.ic_profile_illust1_100 to defaultBbangZipColor.todoRed1_EA7152,
+        R.drawable.ic_profile_illust2_100 to defaultBbangZipColor.todoYellow1_FED45C,
+        R.drawable.ic_profile_illust3_100 to defaultBbangZipColor.todoPurple2_B79FE8,
+        R.drawable.ic_profile_illust4_100 to defaultBbangZipColor.todoBlue1_5C62AC,
+        R.drawable.ic_profile_illust5_100 to defaultBbangZipColor.todoGreen1_7A946D,
+        R.drawable.ic_profile_illust6_100 to defaultBbangZipColor.todoRed2_F09C86
+    )
+
+    val DEFAULT_OUTLINE_COLOR = defaultBbangZipColor.staticWhite_FFFFFF
+}
