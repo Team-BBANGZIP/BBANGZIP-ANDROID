55043db79574cbef8422ec6f7f111e0bcf3eb511
kamja0510 <kamja990510@gmail.com>
2026-03-25 10:52:20 +0900

    [chore] #69 kotlin을 2.3.0 버전으로 올리면서 생기는 dependencies 버전 충돌 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.kotlin.ksp) apply false
