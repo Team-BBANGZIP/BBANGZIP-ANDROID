6ced838cb61a79fcb6540d4ec2024bcc7b86b17b
kamja0510 <kamja990510@gmail.com>
2026-04-13 22:15:27 +0900

    [feat] #90 디버그 용 타이머 시간 단축

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/util/TimerConstants.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/util/TimerConstants.kt
@@ -1,6 +1,8 @@
 package org.android.bbangzip.presentation.ui.timer.util
 
+import org.android.bbangzip.BuildConfig
+
 object TimerConstants {
-    const val THIRTY_MINUTES = 1800000L
-    const val SIXTY_MINUTES = 3600000L
+    val THIRTY_MINUTES: Long = if (BuildConfig.DEBUG) 10_000L else 1_800_000L
+    val SIXTY_MINUTES: Long = if (BuildConfig.DEBUG) 20_000L else 3_600_000L
 }
