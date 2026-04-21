05dd1dd2b86968ac65ce9fc8b0c4cd0bd63e989e
kamja0510 <kamja990510@gmail.com>
2026-04-22 04:26:09 +0900

    [feat] #100 lottie 적용

--- app/src/main/java/org/android/bbangzip/presentation/ui/timer/component/bottomsheet/CompleteBottomSheet.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/timer/component/bottomsheet/CompleteBottomSheet.kt
@@ -1,13 +1,15 @@
 package org.android.bbangzip.presentation.ui.timer.component.bottomsheet
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 import org.android.bbangzip.R
 
 @Composable
@@ -32,6 +34,8 @@ fun CompleteBottomSheet(
         } else {
             stringResource(R.string.complete_sheet_sub_title_n2)
         }
+    val rawRes = if (timeOptionIndex == 0) R.raw.one_bread_success_lottie else R.raw.two_bread_success_lottie
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(rawRes))
     TimerActionBottomSheet(
         isBottomSheetVisible = iisBottomSheetVisible,
         titleText = stringResource(R.string.complete_sheet_title),
@@ -41,25 +45,14 @@ fun CompleteBottomSheet(
         leftBtnIcon = R.drawable.ic_plus_default_24,
         rightBtnIcon = R.drawable.ic_book_default_24,
         content = {
-            if (timeOptionIndex == 0) {
-                Image(
-                    painter = painterResource(id = R.drawable.img_shine_bread_n1),
-                    contentDescription = null,
-                    modifier =
-                        Modifier
-                            .padding(horizontal = 20.dp)
-                            .fillMaxWidth(),
-                )
-            } else {
-                Image(
-                    painter = painterResource(id = R.drawable.img_shine_bread_n2),
-                    contentDescription = null,
-                    modifier =
-                        Modifier
-                            .padding(horizontal = 20.dp)
-                            .fillMaxWidth(),
-                )
-            }
+            LottieAnimation(
+                composition = composition,
+                iterations = 1,
+                modifier =
+                    Modifier
+                        .padding(horizontal = 20.dp)
+                        .fillMaxWidth(),
+            )
         },
         onLeftClick = onRestartBtnClick,
         onRightClick = onCheckTodoBtnClick,
